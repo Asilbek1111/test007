a78f6c82f47f9c0bf7d65a01dd8aeb201a1d52be
SantaClaus111 <99465065+Asilbek1111@users.noreply.github.com>
2024-06-04 16:37:40 +0500

    birinchi versiya

---
+++ index.js
@@ -0,0 +1,38 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var input = prompt("please enter your bithyear");
+// var age = 2024 - input;
+// alert(age)
+
+
+// var input  =  +prompt("please enter the first value")
+// var input2  =  +prompt("please enter the second value")
+// var plus = input + input2;
+// alert(plus)
+
+
+
+// concatination
